6a5118dc30fea872c1f3af76cdd759bf3dc63cc9
Daniel <danielh839@gmail.com>
2020-06-07 13:45:40 -0400

    Added compiled version for heroku

---
+++ dist/server.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const apollo_server_1 = require("apollo-server");
+const schema = apollo_server_1.gql `
+    type Query {
+        hello: String!
+    }
+`;
+const resolvers = {
+    Query: {
+        hello: () => 'world'
+    }
+};
+const server = new apollo_server_1.ApolloServer({ typeDefs: schema, resolvers: resolvers });
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+});
