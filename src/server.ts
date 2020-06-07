d111bb9d6d02d0fab53e5746f3601257a78f0313
Daniel <danielh839@gmail.com>
2020-06-07 13:36:24 -0400

    Initial Commit

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import { ApolloServer, gql } from 'apollo-server';
+
+const schema = gql`
+    type Query {
+        hello: String!
+    }
+`
+
+const resolvers = {
+    Query: {
+        hello: () => 'world'
+    }
+}
+
+const server = new ApolloServer({ typeDefs: schema, resolvers: resolvers });
+
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+});
